3b8a82f878678c7341e5403e91da9cbe893289b9
Corno Schraverus <corno@schraverus.com>
2026-02-10 15:53:33 +0100

    pdt(broken): ...

--- pub/src/implementation/manual/transformers/module/fountain_pen.ts
+++ pub/src/implementation/manual/transformers/module/fountain_pen.ts
@@ -1,14 +1,13 @@
 import * as _p from 'pareto-core/dist/assign'
 
 import * as d_in from "pareto-liana/dist/interface/generated/liana/schemas/module/data/resolved"
-import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
+import * as d_out from "pareto-"
 
 //dependecies
 
 import * as t_liana_module_to_documentation from "./documentation"
 import * as t_liana_module_to_graphviz from "./graphviz"
 import * as t_liana_module_to_lionweb from "./lionweb"
-import * as t_liana_module_to_typescript from "./typescript"
 
 
 export const Module = (
@@ -18,7 +17,6 @@ export const Module = (
         | ['documentation', null]
         | ['graphviz', null]
         | ['lionweb', null]
-        | ['typescript', null]
     }
 
 ): d_out.Directory => {
@@ -28,7 +26,6 @@ export const Module = (
             case 'documentation': return _p.ss($, ($) => t_liana_module_to_documentation.Module(module))
             case 'graphviz': return _p.ss($, ($) => t_liana_module_to_graphviz.Module(module, { 'graph name': "schema" }))
             case 'lionweb': return _p.ss($, ($) => t_liana_module_to_lionweb.Module(module))
-            case 'typescript': return _p.ss($, ($) => t_liana_module_to_typescript.Module(module))
             default: return _p.au($[0])
         }
     })
