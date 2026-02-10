49dd12174daf8a143d8cd283545f0754aa233a7f
Corno Schraverus <corno@schraverus.com>
2026-02-10 14:07:42 +0100

    pdt(broken): ...

--- pub/src/modules/lionweb/implementation/manual/transformers/serialization_chunk/fountain_pen.ts
+++ pub/src/modules/lionweb/implementation/manual/transformers/serialization_chunk/fountain_pen.ts
@@ -3,7 +3,7 @@ import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schem
 
 //dependencies
 import * as t_json_to_fountain_pen_block from "pareto-json/dist/implementation/manual/transformers/json/fountain_pen"
-import * as t_lionweb_to_json from "./transformers/json"
+import * as t_lionweb_to_json from "./json"
 
 
 export const SerializationChunks = ($: d_in.SerializationChunks): d_out.Paragraph => t_json_to_fountain_pen_block.Document(t_lionweb_to_json.SerializationChunks($))
