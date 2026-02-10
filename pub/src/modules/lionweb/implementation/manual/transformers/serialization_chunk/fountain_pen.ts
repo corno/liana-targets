ccdd136eca5cb48c59d308f354fd26c9f6c52c2f
Corno Schraverus <corno@schraverus.com>
2026-02-10 11:41:44 +0100

    pdt(broken): ...

--- pub/src/modules/lionweb/implementation/manual/transformers/serialization_chunk/fountain_pen.ts
+++ pub/src/modules/lionweb/implementation/manual/transformers/serialization_chunk/fountain_pen.ts
@@ -2,7 +2,7 @@ import * as d_in from "../../../../../../interface/generated/liana/schemas/lionw
 import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
 
 //dependencies
-import * as t_json_to_fountain_pen_block from "pareto-json/dist/implementation/manual/schemas/json/transformers/fountain_pen"
+import * as t_json_to_fountain_pen_block from "pareto-json/dist/implementation/manual/transformers/json/fountain_pen"
 import * as t_lionweb_to_json from "./transformers/json"
 
 
