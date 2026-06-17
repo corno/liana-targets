72a66aca16f23360fe8b837b7224e313b5034f58
Corno Schraverus <corno@schraverus.com>
2026-06-17 13:49:34 +0200

    pdt: ...

--- typescript/lib/src/interface/data/alan_light_package.ts
+++ typescript/lib/src/interface/data/alan_light_package.ts
@@ -1,9 +1,9 @@
-import * as p_di from 'pareto-core/dist/interface/data'
+import * as p_ from 'pareto-core/dist/interface/data'
 
 import * as d_alan_light from "../generated/liana/schemas/alan_light/data"
 
 
-export type Package = p_di.Dictionary<Node>
+export type Package = p_.Dictionary<Node>
 
 export type Node =
     | ['model', d_alan_light.Root]
