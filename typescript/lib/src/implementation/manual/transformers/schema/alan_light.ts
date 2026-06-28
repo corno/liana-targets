172b433bbba4598a10521f0658d0f1d208355349
Corno Schraverus <corno@schraverus.com>
2026-06-28 12:30:39 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/transformers/schema/alan_light.ts
+++
@@ -1,6 +0,0 @@
-
-//data types
-import * as d_in from "pareto-liana/dist/interface/generated/liana/schemas/schema/data/resolved"
-import * as d_out from "../../../../interface/generated/liana/schemas/alan_light/data"
-
-// //dependencies
