e31096977952c9a3d29da2adfeebd2e9f54ec89a
Corno Schraverus <corno@schraverus.com>
2026-07-08 13:44:08 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/schema/lionweb_serialization_chunk.ts
+++ typescript/lib/src/interface/declarations/transformers/schema/lionweb_serialization_chunk.ts
@@ -1,7 +1,5 @@
 
 import type * as p_ from 'pareto-core/interface/transformer'
-import p_unreachable_code_path from 'pareto-core/implementation/transformer/specials/unreachable_code_path'
-import p_implement_me from 'pareto-core-dev/implement_me'
 
 //data types
 import type * as d_in from "pareto-liana/interface/generated/liana/schemas/schema/data/resolved"
