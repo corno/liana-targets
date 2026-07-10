486f48f087dc52b11bc86b8502b475224e1fc8fb
Corno Schraverus <corno@schraverus.com>
2026-07-10 10:05:49 +0200

    pdt: ...

--- typescript/lib/src/declarations/refiners/alan_light/schema.ts
+++ typescript/lib/src/declarations/refiners/alan_light/schema.ts
@@ -3,7 +3,7 @@ import type * as p_ from 'pareto-core/interface/refiner'
 
 //data types
 import type * as d_in from "pareto-liana/interface/generated/liana/schemas/schema/data/resolved"
-import type * as d_out from "../../../interface/generated/liana/schemas/alan_light/data.js"
+import type * as d_out from "../../../interface/data/alan_light.js"
 import type * as d_out_package from "../../../interface/data/alan_light_package.js"
 
 export namespace d_function {
--- typescript/lib/src/interface/data/alan_light_package.ts
+++ typescript/lib/src/interface/data/alan_light_package.ts
@@ -1,6 +1,6 @@
 import * as p_ from 'pareto-core/interface/data'
 
-import type * as d_alan_light from "../generated/liana/schemas/alan_light/data.js"
+import type * as d_alan_light from "./alan_light.js"
 
 
 export type Package = p_.Dictionary<Node>
